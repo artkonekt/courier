3ed7735cc3f6af3f3dd2bc3e3c7550f3c95629e1
Hunor Kedves <kedves@users.noreply.github.com>
2016-10-28 10:30:36 +0300

    Added display texts to PackageState enum

--- FanCourier/Transaction/TrackPackage/PackageState.php
+++ FanCourier/Transaction/TrackPackage/PackageState.php
@@ -49,4 +49,22 @@ class PackageState extends Enum
     const ADRESA_GRESITA = 42;
 
     const RETUR = 43;
+    
+    protected static $displayTexts = [
+        self::UNKNOWN                               => "Unknown",
+        self::AWB_NU_A_FOST_PREDAT_CATRE_FANCOURIER => "AWB-ul nu a fost predat catre FAN Courier",
+        self::EXPEDITIE_IN_LIVRARE                  => "Expeditie in livrare",
+        self::LIVRAT                                => "Livrat",
+        self::AVIZAT                                => "Avizat",
+        self::REFUZ_PRIMIRE                         => "Refuz primire",
+        self::REFUZ_PLATA_TRANSPORT                 => "Refuz plata transport",
+        self::LIVRARE_DIN_SEDIUL_FAN_COURIER        => "Livrare din sediul FAN Courier",
+        self::REDIRECTIONAT                         => "Redirectionat",
+        self::CONTACTAT_LIVRARE_ULTERIOARA          => "Contactat, livrare ulterioara",
+        self::RESTRICTII_ACCES_LA_ADRESA            => "Restrictii acces la adresa",
+        self::REFUZ_PLATA_RAMBURS                   => "Refuz plata ramburs",
+        self::AWB_NEEXPEDIAT                        => "AWB neexpediat",
+        self::ADRESA_GRESITA                        => "Adresa gresita",
+        self::RETUR                                 => "Retur"
+    ];
 }
